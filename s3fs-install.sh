4a888ae6a42523429f321bae7ab9287f9f42d253
stupidboy2979 <stupidboy2979@qq.com>
2019-11-14 11:09:07 +0800

    Create s3fs-install.sh

---
+++ s3fs-install.sh
@@ -0,0 +1,8 @@
+sudo yum install -y automake fuse fuse-devel gcc-c++ git libcurl-devel libxml2-devel make openssl-devel
+git clone https://github.com/s3fs-fuse/s3fs-fuse.git
+cd s3fs-fuse
+./autogen.sh
+./configure
+make
+make install
+s3fs --v
