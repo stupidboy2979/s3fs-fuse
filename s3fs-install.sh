61f12eddde9ec9795b1b28013d79d9db073d29bd
stupidboy2979 <stupidboy2979@qq.com>
2019-11-14 11:26:21 +0800

    Update s3fs-install.sh

--- s3fs-install.sh
+++ s3fs-install.sh
@@ -6,3 +6,8 @@ cd s3fs-fuse
 make
 make install
 s3fs --v
+echo "echo ACCESS_KEY_ID:SECRET_ACCESS_KEY > ${HOME}/.passwd-s3fs"
+echo "chmod 600 ${HOME}/.passwd-s3fs"
+echo "mount script"
+echo "mkdir /s3mnt"
+echo "s3fs oos-zhangkun /s3mnt -o passwd_file=${HOME}/.passwd-s3fs -o url=http://oos-cn.ctyunapi.cn -o allow_other -o umask=0 -o max_write=131072 -o use_cache=/dev/shm -o big_writes -o enable_noobj_cache -o sigv2 -o del_cache"
